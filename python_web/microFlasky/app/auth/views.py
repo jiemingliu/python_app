89f91c46b1dc5962a239ce96afc3dc0741dc03bb
jiemingliu <liumingjie.blog@gmail.com>
2017-12-29 17:45:49 +0800

    the other Blueprint

--- python_web/microFlasky/app/auth/views.py
+++ python_web/microFlasky/app/auth/views.py
@@ -1,6 +1,25 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import auth
+from flask_login import logout_user,login_required
 
 @auth.route('/login')
 def login():
 	return render_template('auth/login.html')
+	
+@auth.route('/logout')
+def logout():
+	logout_user()
+	flask('U have been logged out')
+	return redirect(url_for('main.index'))
+	
+@auth.route('/register',methods=['GET','POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data,
+		username=form.username.data,
+		password=form.password.data)
+		db.session.add(user)
+		flash('You can now login.')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form=form)
