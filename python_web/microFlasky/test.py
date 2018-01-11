9c493942a544798d390211c8f7d923d364c42666
jieminglie <liumingjie.blog@gmail.com>
2018-01-11 23:12:27 +0800

    python selenium

--- python_web/microFlasky/test.py
+++ python_web/microFlasky/test.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pytesseract
 from PIL import Image
+import time
 
-image_url = r"http://cloud.jiabenyi.com/admin/auth/code.htm?key=4ea8e987-c9ef-4a7a-99d1-591a34ec74bb"
 driver_url = r"D:\Tools\driverTools\chromedriver.exe"
 network_url = r'http://cloud.jiabenyi.com/admin'
 
@@ -18,12 +18,47 @@ pwd = driver.find_element_by_id('pwd')
 pwd.click()
 pwd.send_keys(666666)
 
+time.sleep(10)
 
-
-image = Image.open(image_url)
-vcode = pytesseract.image_to_string(image)
 code = driver.find_element_by_id('code')
-pwd.click()
-code.send_keys(vcode)
+f = open('test.txt','r')
+codeStr = f.read()
+code.click()
+code.send_keys(codeStr)
+
+newButton = driver.find_element_by_id('newbutton')
+newButton.click()
+
+time.sleep(2)
+
+current_url = driver.current_url
+left_url = current_url.split('#')[0]
+whole_url = left_url+'/#sys_feedback/carConsulting'
+print(whole_url)
+driver.get(whole_url)
+
+goutong = driver.find_element_by_css_selector('button button-success')
+print(goutong)
+print(goutong.tag_name)
+
+# zixun = driver.find_element_by_class_name('nav-item-inner nav-eachinfo')
+# zixun.click()
+
+# time.sleep(2)
+
+# xuechezixun = driver.find_element_by_id('carConsulting')
+# xuechezixun.click()
+
+#codeImg = driver.find_element_by_id('codeImg')
+#image_url = codeImg.get_attribute('src')
+#print(image_url)
+#im = Image.open('code.jpg')
+#stri = pytesseract.image_to_string(im)
+
+
+
+
+
+
 #id1 = driver.find_element_by_class('nav-item-inner nav-eachinfo')
 #print(id1)
